99b3699ccd343296bac6bfabf59cbcaf82602fed
valad47 <VALAD47>
2024-09-29 13:19:53 +0300

    Cleanup instructions header

--- src/instructions.h
+++ src/instructions.h
@@ -1,23 +1,12 @@
 #pragma once
 #include "utils.h"
 
-// First bit //
-
+//Definition of key position
 #define DOWN 1
 #define UP 0
 
-#define MILISECONDS 0
-#define SECONDS 1
-
-// Second bit + 8
-
+//Posible instructions that macro uses
 #define KEYPRESS 1
 #define DELAY 2
-
-// Bit shifts
-
-#define STATEBITSHIFT   0
-#define CMDBITSHIFT     1
-#define VALBITSHIFT     32
 
 instruction_list* parseFile(char *path);
