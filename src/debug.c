556abd196985107544eb1815fda9fe91aaec42ab
valad47 <VALAD47>
2024-10-01 12:26:20 +0300

    Add improved debug output

--- src/debug.c
+++ src/debug.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <time.h>
 #include "debug.h"
+#include "instructions.h"
 
 void printInstructions(instruction_list *list){
     printf("\tState\tCmd\tValue\n");
     while(list!=NULL) {
-        printf("\t%d\t%d\t%d\n", list->state, list->cmd, list->val);
+        char *cmd = NULL, *state = NULL;
+        #define CASE(s, b)\
+            case s:\
+                b = #s;\
+                break;
+
+        switch (list->cmd) {
+            CASE(KEYPRESS, cmd)
+            CASE(DELAY, cmd)
+        }
+        switch (list->state) {
+            CASE(UP, state)
+            CASE(DOWN, state)
+        }
+
+        printf("\t%s\t%s\t%d\n", state, cmd, list->val);
         list = list->next;
     }
     printf("-----------------------------------\n");
